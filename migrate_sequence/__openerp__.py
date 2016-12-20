821ff06db6f7993812e39ee4b0daac67e43b8ecc
Esther Martín <esthermartin001@gmail.com>
2016-12-20 09:35:15 +0100

    [ADD] new module migrate_sequence

---
+++ migrate_sequence/__openerp__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# © 2016 Esther Martín - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+{
+    "name": "Migrate sequences",
+    "version": "8.0.1.0.0",
+    "license": "AGPL-3",
+    "depends": [
+        "base",
+    ],
+    "author": "AvanzOSC",
+    "website": "http://www.avanzosc.es",
+    "contributors": [
+        "Esther Martín <esthermartin@avanzosc.es>",
+    ],
+    "category": "Custom Module",
+    "pre_init_hook": "add_sequence",
+    "installable": True,
+}
